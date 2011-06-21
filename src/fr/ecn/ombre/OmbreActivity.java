be5d9c50acdddfa13ecd7de88c192569b42b8679
Jérôme Vasseur <jerome.vasseur@jhome.fr>
2011-06-21 15:18:49 +0200

    Initial commit : Project layout

---
+++ src/fr/ecn/ombre/OmbreActivity.java
@@ -0,0 +1,13 @@
+package fr.ecn.ombre;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OmbreActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
